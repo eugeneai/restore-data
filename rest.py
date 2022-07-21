fb92f3f4cb732c42a69a2954af8d43707335838e
Evgeny Cherkashin <eugeneai@irnok.net>
2022-07-21 11:23:53 +0800

    Added model exif field to recognition

--- rest.py
+++ rest.py
@@ -1,4 +1,3 @@
-
 from PIL import Image
 from PIL.ExifTags import TAGS
 
@@ -6,7 +5,15 @@ from glob import glob
 import os.path as op
 import os
 
-files = glob('/mnt/data/storage/rest/jpg/f*.jpg')
+SRCDIR = "/mnt/btrfs/restore/tmp"
+TGTDIR = "/mnt/btrfs/restore/tgt"
+
+PAT = op.join(SRCDIR, '.*', '*.jpg')
+
+print(PAT)
+
+files = glob(PAT, recursive=True)
+print(files[:10])
 
 # files = files[:1000]
 
@@ -19,26 +26,29 @@ for file in files:
     path, name = op.split(file)
     if name.startswith('f'):
         # print("EXIF:", name)
-        make = exif.get(0x010f,'NONE')
-        date = exif.get(0x0132,None)
-        # if date is not None:
-        if True:
-            if date is None:
-                date = ""
-            date = date.replace(" ","-").replace(":","-")
-            newname = "{}-{}-{}".format(make,date,name)
-            crs = [ord(c) for c in newname if ord(c)>0]
+        make = exif.get(0x010f, None)
+        model = exif.get(0x0110, None)
+        date = exif.get(0x0132, None)
+        if model is not None or make is not None:
+        # if True:
+            make,model,date = ['' if c is None else c
+                               for c in [make,model,date]]
+            date = date.replace(" ", "-").replace(":", "-")
+            newname = "{}-{}-{}-{}".format(make, model, date, name)
+            crs = [ord(c) for c in newname if ord(c) > 0]
             newname = ''.join([chr(c) for c in crs])
-            newfile = op.join(path,newname)
+            newname = newname.replace(' ','_')
+            newfile = op.join(TGTDIR, newname)
             try:
+                print("RENAME: {}->{}".format(file, newfile))
                 os.rename(file, newfile)
             except ValueError:
                 try:
-                    newname = "GOOD-{}-{}".format(make,name)
-                    newfile = op.join(path,newname)
+                    newname = "GOOD-{}-{}".format(make, name)
+                    newfile = op.join(path, newname)
                     os.rename(file, newfile)
                 except ValueError:
-                    print("CANNOT RENAME:",newfile)
+                    print("CANNOT RENAME:", newfile)
                     print(chr)
             print(newfile)
 
